d5abea73353c6a2e2ef73a92f63146be09b99381
martin11106 <martin-adonis@hotmail.es>
2018-12-03 17:58:58 -0600

    ranking

--- Project Test/joinder-vue-test/src/components/Feed.vue
+++ Project Test/joinder-vue-test/src/components/Feed.vue
@@ -3,34 +3,6 @@
    <navu></navu>
     <v-app light>
       <v-content>
-        <section>
-          <v-parallax
-            src="http://www.escuelamanagement.eu/wp-content/uploads/Pensamiento-cr%C3%ADtico-y-creativo-1080x675.jpg"
-            height="600"
-          >
-            <v-layout column align-center justify-center class="white--text">
-              <h1
-                class="white--text mb-2 display-1 text-xs-center"
-              >Responderemos a todas tus preguntas</h1>
-              <div class="subheading mb-3 text-xs-center">respondemos en menos de 5 minutos</div>
-              <div>
-                <v-card-text>
-                  <v-autocomplete
-                    style="background-color:white;width:220%;margin-left:-55%"
-                    cache-items
-                    flat
-                    hide-no-data
-                    hide-details
-                    label="What state are you from?"
-                    solo-inverted
-                  ></v-autocomplete>
-                </v-card-text>
-              </div>
-              <div></div>
-              <v-btn class="blue lighten-2 mt-5" dark large>Buscar</v-btn>
-            </v-layout>
-          </v-parallax>
-        </section>
 
         <button @click="logout()" class="btn btn-primary">CERRAR SESION</button>
         <br>
